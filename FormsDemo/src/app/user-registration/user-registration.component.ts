d4b7bb1846b6db41377c7a0be34a544e7acf5eb4
keyur2714 <keyur.555kn@gmail.com>
2019-05-11 13:54:57 +0530

    Added Reactive Forms with FormBuilder

--- FormsDemo/src/app/user-registration/user-registration.component.ts
+++ FormsDemo/src/app/user-registration/user-registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration',
@@ -8,28 +8,39 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class UserRegistrationComponent implements OnInit {
 
-  userRegistration : FormGroup;
+  userRegistration;
 
-  constructor() { }
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit() {
     this.createRegistrationForm();
   }
 
   createRegistrationForm():void{
-    this.userRegistration = new FormGroup({
-      userName : new FormControl('',Validators.required),
-      password : new FormControl('',[Validators.required,Validators.minLength(6)]),
-      name : new FormControl('',Validators.required),
-      email : new FormControl('',[Validators.required,Validators.email]),
-      mobileNo : new FormControl('',[Validators.required,Validators.minLength(10),Validators.pattern('^[0-9]+$')]),
-      userRole : new FormControl(),
-      hobbies : new FormArray([new FormControl()])
-    })
+    this.userRegistration = this.formBuilder.group(
+      {
+        userName : this.formBuilder.control('',Validators.required),
+        password : this.formBuilder.control('',[Validators.required,Validators.minLength(6)]),
+        name : this.formBuilder.control('',Validators.required),
+        email : this.formBuilder.control('',[Validators.required,Validators.email]),
+        mobileNo : this.formBuilder.control('',[Validators.required,Validators.minLength(10),Validators.pattern('^[0-9]+$')]),
+        userRole : this.formBuilder.control('admin'),
+        hobbies : this.formBuilder.array([this.formBuilder.control('')])                
+      }
+    );
+    // this.userRegistration = new FormGroup({
+    //   userName : new FormControl('',Validators.required),
+    //   password : new FormControl('',[Validators.required,Validators.minLength(6)]),
+    //   name : new FormControl('',Validators.required),
+    //   email : new FormControl('',[Validators.required,Validators.email]),
+    //   mobileNo : new FormControl('',[Validators.required,Validators.minLength(10),Validators.pattern('^[0-9]+$')]),
+    //   userRole : new FormControl('admin'),
+    //   hobbies : new FormArray([new FormControl()])
+    // })
   }  
 
   addNewHobby():void{
-    let newHobbyControl = new FormControl();
+    let newHobbyControl = this.formBuilder.control('');
     (<FormArray>this.userRegistration.get('hobbies')).push(newHobbyControl);
   }
 
